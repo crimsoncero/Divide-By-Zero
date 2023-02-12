6fe34f8fde9cf7844d8ff36735bfe8cf650d89fd
Amit Breiman <93070078+crimsoncero@users.noreply.github.com>
2023-02-12 15:47:09 +0200

    animations

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
     [Header("Component Attachments")]
     [SerializeField] private Rigidbody2D rb2D;
     [SerializeField] private BoxCollider2D ZSwordCollider;
-
+    [SerializeField] private Animator AnimController;
     
     [SerializeField] LayerMask _groundLayer;
   
@@ -49,12 +49,16 @@ public class PlayerController : MonoBehaviour
         // Placeholder horizontal movement
         if (Input.GetKey(KeyCode.D))
         {
+            AnimController.SetBool("IsRunning", true);
             transform.Translate(Vector2.right * speed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector2.left * speed * Time.deltaTime);
         }
+
+        AnimController.SetFloat("CharacterYVelocity", rb2D.velocity.y);
+        AnimController.SetBool("IsGrounded", IsGrounded());
 
     }
 
